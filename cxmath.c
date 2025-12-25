1ebb9ceef0a70df1f465a5198ed38a8194224522
Technocraft5255 <88849811+Technocraft5255@users.noreply.github.com>
2025-12-25 18:59:32 +0100

    Refactor complex and vector functions to use consistent naming conventions

--- cxmath.c
+++ cxmath.c
@@ -1,4 +1,28 @@
+#include <math.h>
+
 #define PI 3.141592653589793
+
+#define c_mod(a, b) _Generic((a), Complex: complex_mod, longComplex: long_complex_mod )(a, b)
+#define c_arg(a) _Generic((a), Complex: complex_arg, longComplex: long_complex_arg )(a)
+#define c_conj(a) _Generic((a), Complex: complex_conj, longComplex: long_complex_conj )(a)
+#define c_square(a) _Generic((a), Complex: complex_square, longComplex: long_complex_square )(a)
+#define c_add(a, b) _Generic((a), Complex: complex_add, longComplex: long_complex_add )(a, b)
+#define c_sub(a, b) _Generic((a), Complex: complex_sub, longComplex: long_complex_sub )(a, b)
+#define c_mult(a, b) _Generic((a), Complex: complex_mult, longComplex: long_complex_mult )(a, b)
+#define c_divi(a, b) _Generic((a), Complex: complex_divi, longComplex: long_complex_divi )(a, b)
+
+#define v2_norm(a) _Generic((a), Vector2: vector2_norm, longVector2: long_vector2_norm )(a)
+#define v2_dot(a, b) _Generic((a), Vector2: vector2_dot, longVector2: long_vector2_dot )(a, b)
+#define v2_add(a, b) _Generic((a), Vector2: vector2_add, longVector2: long_vector2_add )(a, b)
+#define v2_sub(a, b) _Generic((a), Vector2: vector2_sub, longVector2: long_vector2_sub )(a, b)
+#define v2_scale(a, b) _Generic((a), Vector2: vector2_scale, longVector2: long_vector2_scale )(a, b)
+
+#define v3_norm(a) _Generic((a), Vector3: vector3_norm, longVector3: long_vector3_norm )(a)
+#define v3_dot(a, b) _Generic((a), Vector3: vector3_dot, longVector3: long_vector3_dot )(a, b)
+#define v3_add(a, b) _Generic((a), Vector3: vector3_add, longVector3: long_vector3_add )(a, b)
+#define v3_sub(a, b) _Generic((a), Vector3: vector3_sub, longVector3: long_vector3_sub )(a, b)
+#define v3_scale(a, b) _Generic((a), Vector3: vector3_scale, longVector3: long_vector3_scale )(a, b)
+
 
 long double sqrt_newton(long double x) {
     long double y = x;
@@ -42,50 +66,54 @@ typedef struct{
 } longVector3;
 
 
-double c_mod(Complex c1){
+double complex_mod(Complex c1){
     return (double){
         sqrt_newton(c1.real * c1.real + c1.imag * c1.imag)
     };
 }
 
-double c_arg(Complex c1){} // TODO : Finish this shit
+double complex_arg(Complex c1){
+    return (double){
+        atan2(c1.imag, c1.real)
+    };
+}
 
-Complex c_conj(Complex c1){
+Complex complex_conj(Complex c1){
     return (Complex){
         c1.real,
         -c1.imag
     };
 }
 
-Complex c_square(Complex c1){
+Complex complex_square(Complex c1){
     return (Complex){
         (c1.real * c1.real) - (c1.imag * c1.imag),
         2 * c1.real * c1.imag
     };
 }
 
-Complex c_add(Complex c1, Complex c2){
+Complex complex_add(Complex c1, Complex c2){
     return (Complex){
         c1.real + c2.real,
         c1.imag + c2.imag
     };
 }
 
-Complex c_sub(Complex c1, Complex c2){
+Complex complex_sub(Complex c1, Complex c2){
     return (Complex){
         c1.real - c2.real,
         c1.imag - c2.imag
     };
 }
 
-Complex c_mult(Complex c1, Complex c2){
+Complex complex_mult(Complex c1, Complex c2){
     return (Complex){
         (c1.real*c2.real) - (c1.imag*c2.imag),
         (c1.imag*c2.real) + (c1.real*c2.imag)
     };
 }
 
-Complex c_divi(Complex c1, Complex c2){
+Complex complex_divi(Complex c1, Complex c2){
     return (Complex){
         ((c1.real * c2.real) + (c1.imag * c2.imag))/(c2.real * c2.real + c2.imag * c2.imag),
         ((c1.imag * c2.real) - (c1.real * c2.imag))/(c2.real * c2.real + c2.imag * c2.imag)
@@ -93,50 +121,54 @@ Complex c_divi(Complex c1, Complex c2){
 }
 
 
-long double lc_mod(longComplex c1){
+long double long_complex_mod(longComplex c1){
     return (long double){
         sqrt_newton(c1.real * c1.real + c1.imag * c1.imag)
     };
 }
 
-long double lc_arg(longComplex c1){} // TODO : Finish this shit
+long double long_complex_arg(longComplex c1){
+    return (long double){
+        atan2(c1.imag, c1.real)
+    };
+}
 
-longComplex lc_conj(longComplex c1){
+longComplex long_complex_conj(longComplex c1){
     return (longComplex){
         c1.real,
         -c1.imag
     };
 }
 
-longComplex lc_square(longComplex c1){
+longComplex long_complex_square(longComplex c1){
     return (longComplex){
         (c1.real * c1.real) - (c1.imag * c1.imag),
         2 * c1.real * c1.imag
     };
 }
 
-longComplex lc_add(longComplex c1, longComplex c2){
+longComplex long_complex_add(longComplex c1, longComplex c2){
     return (longComplex){
         c1.real + c2.real,
         c1.imag + c2.imag
     };
 }
 
-longComplex lc_sub(longComplex c1, longComplex c2){
+longComplex long_complex_sub(longComplex c1, longComplex c2){
     return (longComplex){
         c1.real - c2.real,
         c1.imag - c2.imag
     };
 }
 
-longComplex lc_mult(longComplex c1, longComplex c2){
+longComplex long_complex_mult(longComplex c1, longComplex c2){
     return (longComplex){
         (c1.real*c2.real) - (c1.imag*c2.imag),
         (c1.imag*c2.real) + (c1.real*c2.imag)
     };
 }
 
-longComplex lc_divi(longComplex c1, longComplex c2){
+longComplex long_complex_divi(longComplex c1, longComplex c2){
     return (longComplex){
         ((c1.real * c2.real) + (c1.imag * c2.imag))/(c2.real * c2.real + c2.imag * c2.imag),
         ((c1.imag * c2.real) - (c1.real * c2.imag))/(c2.real * c2.real + c2.imag * c2.imag)
@@ -144,33 +176,33 @@ longComplex lc_divi(longComplex c1, longComplex c2){
 }
 
 
-double v_norm(Vector2 u){
+double vector2_norm(Vector2 u){
     return (double){
         sqrt_newton((u.x * u.x) + (u.y * u.y))
     };
 }
 
-double v_dot(Vector2 u, Vector2 v){
+double vector2_dot(Vector2 u, Vector2 v){
     return (double){
         (u.x * v.x) + (u.y * v.y)
     };
 }
 
-Vector2 v_add(Vector2 u, Vector2 v){
+Vector2 vector2_add(Vector2 u, Vector2 v){
     return (Vector2){
         u.x + v.x,
         u.y + v.y
     };
 }
 
-Vector2 v_sub(Vector2 u, Vector2 v){
+Vector2 vector2_sub(Vector2 u, Vector2 v){
     return (Vector2){
         u.x - v.x,
         u.y - v.y
     };
 }
 
-Vector2 v_scale(Vector2 u, double k){
+Vector2 vector2_scale(Vector2 u, double k){
     return (Vector2){
         u.x * k,
         u.y * k
@@ -178,33 +210,33 @@ Vector2 v_scale(Vector2 u, double k){
 }
 
 
-long double lv_norm(longVector2 u){
+long double longv_norm(longVector2 u){
     return (long double){
         sqrt_newton((u.x * u.x) + (u.y * u.y))
     };
 }
 
-long double lv_dot(longVector2 u, longVector2 v){
+long double longvector2_dot(longVector2 u, longVector2 v){
     return (long double){
         (u.x * v.x) + (u.y * v.y)
     };
 }
 
-longVector2 lv_add(longVector2 u, longVector2 v){
+longVector2 longvector2_add(longVector2 u, longVector2 v){
     return (longVector2){
         u.x + v.x,
         u.y + v.y
     };
 }
 
-longVector2 lv_sub(longVector2 u, longVector2 v){
+longVector2 longvector2_sub(longVector2 u, longVector2 v){
     return (longVector2){
         u.x - v.x,
         u.y - v.y
     };
 }
 
-longVector2 lv_scale(longVector2 u, long double k){
+longVector2 longvector2_scale(longVector2 u, long double k){
     return (longVector2){
         u.x * k,
         u.y * k
@@ -212,19 +244,19 @@ longVector2 lv_scale(longVector2 u, long double k){
 }
 
 
-double v3_norm(Vector3 u){
+double vector3_norm(Vector3 u){
     return (double){
         sqrt_newton((u.x * u.x) + (u.y * u.y) + (u.z * u.z))
     };
 }
 
-double v3_dot(Vector3 u, Vector3 v){
+double vector3_dot(Vector3 u, Vector3 v){
     return (double){
         (u.x * v.x) + (u.y * v.y) + (u.z * v.z)
     };
 }
 
-Vector3 v3_add(Vector3 u, Vector3 v){
+Vector3 vector3_add(Vector3 u, Vector3 v){
     return (Vector3){
         u.x + v.x,
         u.y + v.y,
@@ -232,7 +264,7 @@ Vector3 v3_add(Vector3 u, Vector3 v){
     };
 }
 
-Vector3 v3_sub(Vector3 u, Vector3 v){
+Vector3 vector3_sub(Vector3 u, Vector3 v){
     return (Vector3){
         u.x - v.x,
         u.y - v.y,
@@ -240,7 +272,7 @@ Vector3 v3_sub(Vector3 u, Vector3 v){
     };
 }
 
-Vector3 v3_prod(Vector3 u, Vector3 v){
+Vector3 vector3_prod(Vector3 u, Vector3 v){
     return (Vector3){
         (u.y * v.z) - (u.z * v.y),
         (u.z * v.x) - (u.x * v.z),
@@ -248,7 +280,7 @@ Vector3 v3_prod(Vector3 u, Vector3 v){
     };
 }
 
-Vector3 v3_scale(Vector3 u, double k){
+Vector3 vector3_scale(Vector3 u, double k){
     return (Vector3){
         u.x * k,
         u.y * k,
